e6c85513fb62fb9c285347a521d6ecda5910c793
elpekenin <elpekenin@elpekenin.dev>
2023-10-07 01:08:22 +0200

    Remove leftover `KEYMAP_LAYERS_H` configuration

--- keyboards/elpekenin/access/keymaps/elpekenin/rules.mk
+++ keyboards/elpekenin/access/keymaps/elpekenin/rules.mk
@@ -3,6 +3,3 @@ TOUCH_SCREEN = yes
 UCIS_ENABLE = yes
 WPM_ENABLE = yes
 XAP_ENABLE = yes
-
-KEYMAP_LAYERS_H = keyboards/elpekenin/access/keymaps/elpekenin/layers.h
-OPT_DEFS += -DKEYMAP_LAYERS_H=\"$(KEYMAP_LAYERS_H)\"
