48cfa67d8353e3fd93e82dd0a0150347050ced80
elpekenin <elpekenin@elpekenin.dev>
2023-12-03 20:42:29 +0100

    `mkdir -p` on downloads

--- src/operations/wget.rs
+++ src/operations/wget.rs
@@ -2,25 +2,32 @@ use super::prelude::*;
 
 #[derive(Clone, Debug, Deserialize, JsonSchema)]
 pub struct Wget {
-    url: String,      // file to download (raw file, not HTML :P)
-    filename: String, // to save the file (located at build::Settings.path / DOWNLOADS)
+    url: String,  // file to download (raw file, not HTML :P)
+    path: String, // where to save the file (relative to build::Settings.path), eg: users/elpekenin/downloads/rtc.c
 }
-
-const DOWNLOADS: &str = "downloads";
 
 impl OperationTrait for Wget {
     fn apply(&self, settings: &build::Settings, repository: &git::Repository) {
-        repository.run(format!("mkdir -p {DOWNLOADS}"), true);
+        let full_path = format!("./{}", self.path);
+        let parent = std::path::Path::new(&full_path)
+            .parent()
+            .expect("Did not get a valid path");
+
         repository.run(
-            format!("wget {} -O {DOWNLOADS}/{}", self.url, self.filename),
+            format!(
+                "mkdir -p {} && wget {} -O {}",
+                parent.to_string_lossy(),
+                self.url,
+                self.path
+            ),
             true,
         );
     }
 
     fn message(&self) -> String {
         format!(
-            "Downloading <blue>{}</> <green>-></> <blue>{DOWNLOADS}/{}</>",
-            self.url, self.filename
+            "Downloading <blue>{}</> <green>-></> <blue>{}</>",
+            self.url, self.path
         )
     }
 }
